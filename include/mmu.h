6728df4e76441458f8f833b2fc6a33fc8e38f2bb
Sebastián <sgq995@gmail.com>
2019-07-09 19:52:18 -0500

    feat: MMU was added

---
+++ include/mmu.h
@@ -0,0 +1,36 @@
+#ifndef MMU_H_
+#define MMU_H_
+
+#include <stdint.h>
+
+#define MMU_MEMORY_SIZE (4096)
+
+#define MMU_INTERPRETER_START (0x000)
+#define MMU_INTERPRETER_END   (0x1FF)
+
+#define MMU_BUILTIN_FONT_START (0x050)
+#define MMU_BUILTIN_FONT_END   (0x0A0)
+
+#define MMU_PROGRAM_START (0x200)
+
+#define MMU_CALL_STACK_START (0xEA0)
+#define MMU_CALL_STACK_END   (0xEFF)
+
+#define MMU_DISPLAY_REFRESH_START (0xF00)
+#define MMU_DISPLAY_REFRESH_END   (0xFFF)
+
+typedef struct mmu {
+  uint8_t memory[MMU_MEMORY_SIZE];
+} mmu_t;
+
+void mmu_init(mmu_t *mmu);
+
+uint8_t mmu_load_8bit(mmu_t *mmu, uint16_t address);
+
+void mmu_save_8bit(mmu_t *mmu, uint16_t address, uint8_t value);
+
+uint16_t mmu_load_16bit_le(mmu_t *mmu, uint16_t address);
+
+void mmu_save_16bit_le(mmu_t *mmu, uint16_t address, uint16_t value);
+
+#endif  /* MMU_H_ */
---
+++ src/mmu.c
@@ -0,0 +1,41 @@
+#include "mmu.h"
+
+#include <assert.h>
+#include <string.h>
+
+void mmu_init(mmu_t *mmu) {
+  assert(mmu != NULL);
+
+  memset(mmu->memory, 0, sizeof(mmu->memory));
+
+  // TODO: load fontset
+}
+
+uint8_t mmu_load_8bit(mmu_t *mmu, uint16_t address) {
+  assert(mmu != NULL);
+  assert(address < sizeof(mmu->memory));
+
+  return mmu->memory[address];
+}
+
+void mmu_save_8bit(mmu_t *mmu, uint16_t address, uint8_t value) {
+  assert(mmu != NULL);
+  assert(address < sizeof(mmu->memory));
+
+  mmu->memory[address] = value;
+}
+
+uint16_t mmu_load_16bit_le(mmu_t *mmu, uint16_t address) {
+  assert(mmu != NULL);
+  assert(address < sizeof(mmu->memory));
+
+  return mmu->memory[address];
+}
+
+void mmu_save_16bit_le(mmu_t *mmu, uint16_t address, uint16_t value) {
+  assert(mmu != NULL);
+  assert(address < sizeof(mmu->memory));
+
+  mmu->memory[address]     = (uint8_t)((value >> 8) & 0xFF);
+  mmu->memory[address + 1] = (uint8_t)((value     ) & 0xFF);
+}
